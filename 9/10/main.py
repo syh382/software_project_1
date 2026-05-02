2d18a337205a64b4ef51d293c560f97d2e8474a6
syh382 <tojonggo@gmail.com>
2026-05-02 12:29:46 +0900

    main.py

--- 9/10/main.py
+++ 9/10/main.py
@@ -1,12 +1,7 @@
 # 10
-n = list(input())
-ex = []
-l = 1
-while True:
-    ex.append(n[-l])
-    l+=1
-    if l > len(n):
-        print("".join(ex))
-        break
-
+n = input()
+l = -1
+while len(n) >= abs(l):
+    print(n[l], end="")
+    l -= 1
 
