97446cae4f177d1173a1cb6b699c5378d75e3478
syh382 <tojonggo@gmail.com>
2026-05-02 12:10:16 +0900

    Delete 10.py

--- 10.py
+++
@@ -1,7 +0,0 @@
-str = "Life is like the ocean; sometimes calm and peaceful, sometimes turbulent and rough. The key is how we navigate the waves. Each challenge we face makes us stronger, wiser, and more resilient. So, never lose your positive spirit, keep hope alive, and move forward. Eventually, every storm will pass, and clear skies will return."
-print(len(str))
-print(str.count("e"))
-print(str.index("v"))
-lis = str.split(".")
-for i in lis:
-    print(i.upper())
